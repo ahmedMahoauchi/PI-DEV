785a069ba52d24dd85bf01de2d775bfd931e8285
mohamed_kalech <61579423+mohamed213455@users.noreply.github.com>
2022-03-11 10:16:49 +0100

    Add files via upload

---
+++ utils/Consts.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Consts {
+
+
+
+    public static final String IMG_PATH_LOAD = "file:C:/Users/AHMED/Desktop/ahmedddddd/src/uploadImages.newImages/";
+    public static final String IMG_PATH = "C:\\Users\\AHMED\\Desktop\\ahmedddddd\\src\\uploadImages.newImages\\";
+
+}
---
+++ utils/PostUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.Random;
+
+public class PostUtils {
+    public String randomString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1).limit(targetStringLength).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        return generatedString;
+    }
+}
